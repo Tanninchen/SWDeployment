05d2e96e0c819243019c4536cb3db958921c2063
Tanninchen <106107@fhwn.ac.at>
2020-11-11 20:48:51 +0100

    add AppInsights

--- swDeploy_Web-app/index.js
+++ swDeploy_Web-app/index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
+const insight = require('applicationinsights')
+appInsights.setup('8983428a-19da-413a-bf1f-731300b1ebe7').start();
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
